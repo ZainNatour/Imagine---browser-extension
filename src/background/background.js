2eb341cdad00abb787265e719ef0f3c5ade733a3
ZainNatour <119789942+ZainNatour@users.noreply.github.com>
2025-07-06 21:52:22 +0300

    chore: remove js duplicates

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,6 @@
 import { isOnlineStore, loadStoreDomains } from "./modules/urlUtils.js";
 import { addToWishlist } from "../popup/modules/wishlist.js";
-import { registerProductListener } from "./onProductDetected.js";
+import { registerProductListener } from "./onProductDetected.ts";
 
 let lastActiveTabId = null;
 
