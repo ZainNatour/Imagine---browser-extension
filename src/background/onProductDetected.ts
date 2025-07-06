8d6615cb91924e7fbecba4c14e885c16c7eace2b
ZainNatour <119789942+ZainNatour@users.noreply.github.com>
2025-07-06 21:27:07 +0300

    Validate sender in product detection messages

--- src/background/onProductDetected.ts
+++ src/background/onProductDetected.ts
@@ -14,7 +14,10 @@ export async function onProductDetected(product: Product | null, similars: Simil
  * Register message listener for product detection events.
  */
 export function registerProductListener() {
-  chrome.runtime.onMessage.addListener((msg) => {
+  chrome.runtime.onMessage.addListener((msg, sender) => {
+    if (sender.id !== chrome.runtime.id) {
+      return;
+    }
     if (msg && msg.type === 'productDetected') {
       void onProductDetected(msg.product, msg.similars);
     }
